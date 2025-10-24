bfd0431ce7fe0ce41a1c41467204c8dc66e39491
ppmrika-source <ppmrika@gmail.com>
2025-10-24 17:16:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -605,10 +605,11 @@ with st.form("form_metadata"):
         ]
 
         # Cek apakah ada kolom yang kosong
-        if any(field.strip() == "" for field in data_fields):
-            st.error("⚠️ Mohon lengkapi semua kolom bertanda * sebelum menyimpan data.")
-        else:
-            # Tambahkan data ke DataFrame
+       if any(field.strip() == "" for field in data_fields):
+    st.error("⚠️ Mohon lengkapi semua kolom bertanda * sebelum menyimpan data.")
+else:
+    # Tambahkan data ke DataFrame
+    UNIT_KERJA = [
     UNIT_KERJA = [
         "Badan Perencanaan Pembangunan Penelitian & Pengembangan Daerah",
         "Badan Pendapatan Daerah",
@@ -756,6 +757,7 @@ elif menu == "Statistik":
 elif menu == "Tentang Aplikasi":
     st.title("ℹ️ Tentang")
     st.write("Aplikasi Bank Data Kemiskinan Kutai Barat - Bappeda Litbang.")
+
 
 
 
