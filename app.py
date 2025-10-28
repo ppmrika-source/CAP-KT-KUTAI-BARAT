d5859ea7241f255c7c4d2cf2aa00180621e1f3ea
ppmrika-source <ppmrika@gmail.com>
2025-10-28 23:29:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -518,19 +518,11 @@ def parse_rupiah(text):
         return float(text)
     except:
         return 0.0
-# -----------------------------
-# Input Jumlah Bantuan
-# -----------------------------
-jumlah_input = st.text_input("Jumlah Bantuan (Rp)")
-jumlah_bantuan = parse_rupiah(jumlah_input)
-st.write(f"Jumlah Bantuan: Rp {jumlah_bantuan:,.2f} (angka float)")
 
-# -----------------------------
-# Input Total PAGU
-# -----------------------------
-pagu_input = st.text_input("Total PAGU (Rp)")
-total_PAGU = parse_rupiah(pagu_input)
-st.write(f"Total PAGU: Rp {total_PAGU:,.2f} (angka float)")
+total_PAGU = parse_rupiah(jumlah_input)
+
+st.write(f"Nilai yang dibaca: {total_PAGU:,.2f} (angka float)")
+
 
 if st.button("💾 Simpan Data"):
         new_data = pd.DataFrame([{
@@ -828,6 +820,7 @@ elif menu == "Statistik":
 elif menu == "Tentang Aplikasi":
     st.title("ℹ️ Tentang")
     st.write("Aplikasi Bank Data Kemiskinan Kutai Barat - Bappeda Litbang.")
+
 
 
 
