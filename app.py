df10ce353aaa57691c959bee8567f8d96b8f45ce
ppmrika-source <ppmrika@gmail.com>
2025-10-29 00:13:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -749,7 +749,7 @@ for idx, row in df_upload.iterrows():
                     file_name=row["Nama File Asli"],
                      key=f"download_{idx}"
                 )
-            else:
+                else:
                 st.warning(f"File {row['Nama File Asli']} tidak ditemukan di server.")
     else:
         st.info("Belum ada file yang diupload.")
@@ -839,6 +839,7 @@ elif menu == "Statistik":
 elif menu == "Tentang Aplikasi":
     st.title("ℹ️ Tentang")
     st.write("Aplikasi Bank Data Kemiskinan Kutai Barat - Bappeda Litbang.")
+
 
 
 
