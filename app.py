85fd22e3dc35b3e49ea009399c3ec341ef6a6671
ppmrika-source <ppmrika@gmail.com>
2025-10-28 21:11:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -747,9 +747,9 @@ st.header("📑 Data Bantuan Tersimpan")
 st.dataframe(st.session_state.data_bantuan, use_container_width=True)
 
     # Tombol download
-    if not st.session_state.data_bantuan.empty:
-        csv = st.session_state.data_bantuan.to_csv(index=False).encode("utf-8")
-        st.download_button("⬇️ Download Data (CSV)", csv, "data_bantuan.csv", "text/csv")
+if not st.session_state.data_bantuan.empty:
+    csv = st.session_state.data_bantuan.to_csv(index=False).encode("utf-8")
+    st.download_button("⬇️ Download Data (CSV)", csv, "data_bantuan.csv", "text/csv")
     else:
         st.info("Belum ada data yang tersimpan.")
 
@@ -994,6 +994,7 @@ elif menu == "Statistik":
 elif menu == "Tentang Aplikasi":
     st.title("ℹ️ Tentang")
     st.write("Aplikasi Bank Data Kemiskinan Kutai Barat - Bappeda Litbang.")
+
 
 
 
